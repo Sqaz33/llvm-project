797ccd8490c65c3db6444ad0c1951ae599f53408
Stepan Matveev <mset321@gmail.com>
2026-05-15 20:40:26 +0300

    refactor: my dce

--- llvm/lib/Transforms/Utils/MyDCE.cpp
+++ llvm/lib/Transforms/Utils/MyDCE.cpp
@@ -3,28 +3,44 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <list>
 
 #include "llvm/IR/Function.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/Transforms/Utils/Local.h"
 
 using namespace llvm;
 
 PreservedAnalyses MyDCEPath::run(Function &F,
                                  FunctionAnalysisManager &AM) {
-  std::vector<Instruction*> instrs;
-  bool change = true;
-  while (change) {
-    change = false;
-    for (BasicBlock& bb: F) {
+  std::list<Instruction*> instrs;
+  auto* tli = &AM.getResult<TargetLibraryAnalysis>(F);
+
+  for (BasicBlock& bb: F) {
       for (Instruction& i : bb) {
-        if (i.use_empty() && !i.mayHaveSideEffects() && !i.isTerminator()) {
-          change = true;
+        if (i.use_empty() && isInstructionTriviallyDead(&i, tli)) {
           instrs.push_back(&i);
+      }
+    }
+  }
+
+  for (auto it = instrs.begin(); it != instrs.end(); it = instrs.erase(it)) {
+    auto* i = *it;
+    
+    unsigned lim = i->getNumOperands();
+    for (unsigned j = 0; j < lim; ++j) {
+      auto* op = i->getOperand(j);
+      i->setOperand(j, nullptr);
+      if (op->use_empty() && op != i) {
+        if (auto* iop = dyn_cast<Instruction>(op)) {
+          if (isInstructionTriviallyDead(iop, tli)) {
+            instrs.push_back(iop);
+          }
         }
       }
     }
-    for (auto* i : instrs) i->eraseFromParent();
-    instrs.clear();
+
+    i->eraseFromParent();
   }
 
   return PreservedAnalyses::all();
